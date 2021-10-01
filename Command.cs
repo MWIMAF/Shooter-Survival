b0375ae190abda3e19f333e60d2b8f2f9f957594
MWIMAF <79455703+MWIMAF@users.noreply.github.com>
2021-10-01 23:03:43 +0700

    Add files via upload

---
+++ Command.cs
@@ -0,0 +1,8 @@
+﻿namespace Command
+{
+    public abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+}
---
+++ EnemyFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyFactory : MonoBehaviour, IFactory
+{
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+
+    public GameObject FactoryMethod(int tag, Transform spawnPoint)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag], spawnPoint.position, Quaternion.identity);
+        return enemy;
+    }
+}
